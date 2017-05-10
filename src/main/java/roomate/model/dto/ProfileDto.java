798b3324c4f6b28563894adaa8f51403fa38b6b4
katarzyna.sowa <katarzynasowa11@gmail.com>
2017-05-10 21:12:34 +0200

    mapstruct init

---
+++ src/main/java/roomate/model/dto/ProfileDto.java
@@ -0,0 +1,24 @@
+package roomate.model.dto;
+
+import lombok.Data;
+
+@Data
+public class ProfileDto {
+
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String sex;
+
+    private String workTime;
+    private String studiesTime;
+
+    private Boolean acceptAnimals;
+    private Boolean acceptSmoking;
+    private Boolean acceptDrinking;
+
+    private Boolean cleaningSchedule;
+
+    private String aboutMe;
+
+}
