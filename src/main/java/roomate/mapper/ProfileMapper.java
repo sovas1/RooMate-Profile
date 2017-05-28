f391dab506a4764501fd59cf8bb0a196e1f4e4bb
katarzyna.sowa <katarzynasowa11@gmail.com>
2017-05-28 10:06:43 +0200

    map<enum,string>

--- src/main/java/roomate/mapper/ProfileMapper.java
+++ src/main/java/roomate/mapper/ProfileMapper.java
@@ -7,22 +7,7 @@ import roomate.model.dto.ProfileDto;
 @Mapper(componentModel = "spring")
 public interface ProfileMapper {
 
-//    @Mappings({
-//            @Mapping(source = "work", target = "workTime"),
-//            @Mapping(source = "studies", target = "studiesTime"),
-//            @Mapping(source = "animals", target = "acceptAnimals"),
-//            @Mapping(source = "smoking", target = "acceptSmoking"),
-//            @Mapping(source = "drinking", target = "acceptDrinking")
-//    })
     ProfileDto toDto(Profile profile);
-
-//    @Mappings({
-//            @Mapping(source = "workTime", target = "work"),
-//            @Mapping(source = "studiesTime", target = "studies"),
-//            @Mapping(source = "acceptAnimals", target = "animals"),
-//            @Mapping(source = "acceptSmoking", target = "smoking"),
-//            @Mapping(source = "acceptDrinking", target = "drinking")
-//    })
     Profile toEntity(ProfileDto profile);
 
 }
